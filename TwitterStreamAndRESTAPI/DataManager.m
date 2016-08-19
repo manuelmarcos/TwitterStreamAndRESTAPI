2dcff7a1bd35e02b91e0c73670b5d5d0f66f237a
Manuel Marcos Regalado <manuel@ribot.co.uk>
2016-08-19 11:19:31 +0100

    Add Data Manager

---
+++ TwitterStreamAndRESTAPI/DataManager.m
@@ -0,0 +1,13 @@
+//
+//  DataManager.m
+//  TwitterStreamAndRESTAPI
+//
+//  Created by Manuel Marcos Regalado on 19/08/2016.
+//  Copyright © 2016 Manuel Marcos Regalado. All rights reserved.
+//
+
+#import "DataManager.h"
+
+@implementation DataManager
+
+@end
