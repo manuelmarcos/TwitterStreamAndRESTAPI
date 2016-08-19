fba9b3d196ba6cb2e05bc772e7ca6b79400be785
Manuel Marcos Regalado <manuel@ribot.co.uk>
2016-08-19 09:34:40 +0100

    Add Network category for subclass only

---
+++ TwitterStreamAndRESTAPI/NetworkManager+ForSubclassEyesOnly.h
@@ -0,0 +1,13 @@
+//
+//  NetworkManager+ForSubclassEyesOnly.h
+//  TwitterStreamAndRESTAPI
+//
+//  Created by Manuel Marcos Regalado on 19/08/2016.
+//  Copyright © 2016 Manuel Marcos Regalado. All rights reserved.
+//
+
+#import "NetworkManager.h"
+
+@interface NetworkManager (ForSubclassEyesOnly)
+
+@end
---
+++ TwitterStreamAndRESTAPI/NetworkManager+ForSubclassEyesOnly.m
@@ -0,0 +1,13 @@
+//
+//  NetworkManager+ForSubclassEyesOnly.m
+//  TwitterStreamAndRESTAPI
+//
+//  Created by Manuel Marcos Regalado on 19/08/2016.
+//  Copyright © 2016 Manuel Marcos Regalado. All rights reserved.
+//
+
+#import "NetworkManager+ForSubclassEyesOnly.h"
+
+@implementation NetworkManager (ForSubclassEyesOnly)
+
+@end
